19adff7d600b7460ba0b744a239959eedc12b125
Nattapatque <nattapatque@gmail.com>
2024-04-24 22:38:14 +0700

    fix months

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const dayOfTheWeek = (date = new Date()) => {
 }
 
 const monthOfTheYear = (date = new Date()) => {
-    const days = ['January' , 'February' , 'March' , 'April' , 'May' , 'June' , 'July' , 'August' , 'September' , 'October' , 'November' , 'December'];
+    const months = ['January' , 'February' , 'March' , 'April' , 'May' , 'June' , 'July' , 'August' , 'September' , 'October' , 'November' , 'December'];
 
     return months[date.getMonth()];
 }
